108b67bd91dab9f8537516680688718b353e6b4c
pepipe <pepipee@gmail.com>
2024-09-08 12:12:46 +0100

    added a utility logger class

---
+++ Assets/_Project/Scripts/Utils/CardMatchLogger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace CardMatch.Utils
+{
+    public static class CardMatchLogger
+    {
+        public static bool LoggingEnabled; 
+        
+        public static void Log(string message)
+        {
+            if (!LoggingEnabled) return;
+            Debug.Log(message);
+        }
+        
+        public static void LogWarning(string message)
+        {
+            if (!LoggingEnabled) return;
+            Debug.LogWarning(message);
+        }
+
+        public static void LogError(string message)
+        {
+            if (!LoggingEnabled) return;
+            Debug.LogError(message);
+        }
+    }
+}
