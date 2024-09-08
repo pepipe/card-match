5119a01cf266087df7e473b758e8caf50d9233c6
pepipe <pepipee@gmail.com>
2024-09-08 12:13:04 +0100

    added the skeleton of GameManager to control logging

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using CardMatch.Utils;
+using UnityEngine;
+
+namespace CardMatch
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Header("Debug Settings")]
+        [SerializeField] bool LoggingEnable;
+
+        void Awake()
+        {
+            CardMatchLogger.LoggingEnabled = LoggingEnable;
+        }
+    }
+}
