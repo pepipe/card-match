24e4f6f8286c5e2504c0a754366bbef36b4d2e68
pepipe <pepipee@gmail.com>
2024-09-09 00:08:46 +0100

    Created SoundClip monobehaviour utility to facilitate the addition of sounds

---
+++ Assets/_Project/Scripts/Utils/SoundClip.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace CardMatch.Utils
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundClip : MonoBehaviour
+    {
+        [SerializeField] AudioClip Clip;
+        
+        AudioSource _audioSource;
+
+        void Awake()
+        {
+            _audioSource = gameObject.GetComponent<AudioSource>();
+        }
+
+        void Start()
+        {
+            _audioSource.clip = Clip;
+        }
+
+        public void PlayOneShot()
+        {
+            _audioSource.PlayOneShot(Clip);
+        }
+
+        public void Play()
+        {
+            _audioSource.Play();
+        }
+
+        public void Stop()
+        {
+            _audioSource.Stop();
+        }
+
+        public void SetLoop(bool loop)
+        {
+            _audioSource.loop = loop;
+        }
+
+        public bool IsPlaying()
+        {
+            return _audioSource.isPlaying;
+        }
+    }
+}
